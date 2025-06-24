0f52748f52746b52ea4eb407f136f583fae3fcb5
genga6 <gengaru617science@gmail.com>
2025-06-24 09:21:55 +0000

    changed HMR to work with Docker

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  const handleStart = () => {
+    alert("まだ機能は実装されていません");
+  }
+</script>
+
+<main class="min-h-screen bg-white flex flex-col items-center justify-center px-4 text-center space-y-10">
+  <section>
+    <h1 class="text-4xl sm:text-6xl font-bold mb-4 text-gray-900">hitoQ</h1>
+  <button
+    on:click={handleStart}
+    class="mt-6 bg-blue-600 text-white px-6 py-3 rounded-2xl shadow-md hover:bg-blue-700 transition"
+  >
+    はじめる！
+  </button>
+</section>
+</main>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		host: '0.0.0.0',    // Allow access from outside the container (e.g. devcontainers, Docker)
+		port: 5173,         // Preferred port (should match docker-compose or dev setup)
+		strictPort: true,   // Fail if the specified port is already in use
+		watch: {
+		usePolling: true,   // Enable polling for file changes (needed in Docker or devcontainers)
+		interval: 100,      // Polling interval in milliseconds
+		},
+	},
 	test: {
 		projects: [
 			{
